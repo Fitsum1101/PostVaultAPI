59a24d4efb230d0619ee57d94f0e9ca9dbc5134c
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-11 21:53:22 +0300

    signup and login route validation has been done

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,59 @@
 const express = require("express");
+const { body } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
+const User = require("../Model/user");
 const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/signUp", authController.postSignUp);
+router.post(
+  "/signUp",
+  [
+    body("username").trim().notEmpty(),
+    body("email", "please insert email field")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("please insert valid email")
+      .normalizeEmail()
+      .custom(async (value, { req }) => {
+        const user = await User.findUser(value);
+        if (user[0].length > 0) {
+          throw new Error("Email aleady in use!");
+        }
+      }),
+    body("password", "password field in Empty please insert something")
+      .trim()
+      .notEmpty()
+      .isLength({ min: 5, max: 10 })
+      .withMessage("password needed to be between 5 and 10"),
+  ],
+  authController.postSignUp
+);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("email", "please insert email field")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("please insert valid email")
+      .normalizeEmail()
+      .custom(async (value, { req }) => {
+        const user = await User.findUser(value);
+        if (user[0].length) {
+          throw new Error("Email does not Exsits");
+        }
+      }),
+    body("password")
+      .trim()
+      .notEmpty()
+      .isLength({ min: 5, max: 10 })
+      .withMessage("password needed to be between 5 and 10"),
+  ],
+  authController.postLogin
+);
 
 module.exports = router;
