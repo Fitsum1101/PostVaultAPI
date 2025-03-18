c2eecdb7e13507506813d0b8724122baa9c2a0db
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-18 20:16:17 +0300

    socket io used in it

--- Model/posts.js
+++ Model/posts.js
@@ -13,6 +13,17 @@ module.exports = class Posts {
       [this.content, this.user_id, this.productURL, this.title]
     );
   }
+
+  update(id) {
+    if (!this.productURL)
+      return db.execute(
+        `UPDATE posts SET content ='${this.content}' , title = '${this.title}' WHERE id = ${id} && user_id = ${this.user_id} `
+      );
+    else
+      return db.execute(
+        `UPDATE posts SET content ='${this.content}' , productURL = '${this.productURL.path}', title = '${this.title}' WHERE id = ${id} && user_id = ${this.user_id} `
+      );
+  }
   static fetchAll() {
     return db.execute("SELECT content,title FROM posts");
   }
@@ -23,5 +34,8 @@ module.exports = class Posts {
     return db.execute(
       `DELETE FROM posts WHERE user_id = ${user_id} && id = ${post_id} `
     );
+  }
+  static fetchUserData(id) {
+    return db.execute("SELECT * FROM posts WHERE user_id = " + id);
   }
 };
