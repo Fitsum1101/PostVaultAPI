98dfabd72d60a2a2ff9a5c4f76bafbf585ce4c44
Fitsum Kifle <fitsumt835@gmail.com>
2025-03-18 20:17:31 +0300

    socket.io implementaion

--- controllers/post.js
+++ controllers/post.js
@@ -2,21 +2,19 @@ const { validationResult } = require("express-validator");
 
 const Post = require("../Model/posts");
 const deleteFile = require("../util/file");
+const socket = require("../socket");
 
 exports.createPosts = async (req, res, next) => {
   const result = validationResult(req);
-
-  console.log(req.body);
   const content = req.body.content;
   const title = req.body.title;
   const user = req.user.id;
   const productUrl = req.file.path;
 
-  console.log(content, title, user, productUrl);
-
   const newPost = new Post(content, user, productUrl, title);
   try {
     const result = await newPost.save();
+    socket.emit("posts", { action: "added", post: newPost });
     res.status(201).json({
       msg: "new posts adde to user",
       post: {
@@ -45,6 +43,20 @@ exports.getPosts = async (req, res, next) => {
   }
 };
 
+exports.getUserPosts = async (req, res, next) => {
+  const user_id = req.user.id;
+  try {
+    const userPosts = await Post.fetchUserData(user_id);
+    res.status(200).json({
+      msg: "user data fecthed",
+      posts: userPosts[0],
+    });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
+
 exports.deletePost = async (req, res, next) => {
   const user_id = req.user.id;
   const post_id = req.body.postId;
@@ -58,6 +70,38 @@ exports.deletePost = async (req, res, next) => {
       result,
     });
   } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUpdatePost = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const post = await Post.fetchById(postId);
+    return res.status(200).json({
+      post: post[0][0],
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.putPost = async (req, res, next) => {
+  const postId = req.params.postId;
+  const content = req.body.content;
+  const title = req.body.title;
+  const user = req.user.id;
+  const productUrl = req.file;
+  const updatedPost = new Post(content, user, productUrl, title);
+  try {
+    const result = await updatedPost.update(postId);
+    console.log(result);
+    res.status(201).json({
+      msg: "updated successfuly",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
